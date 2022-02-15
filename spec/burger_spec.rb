c39f1b59e708997d9c8bb524a7fb3ed64e2928fe
JenJen <jennifer7793@gmail.com>
2022-02-15 20:33:05 +0800

    usage of before hook

--- spec/burger_spec.rb
+++ spec/burger_spec.rb
@@ -1,14 +1,28 @@
 RSpec.describe Burger do
+  before do 
+    @burger = Burger.new('Beef', 'Cheddar')
+  end
+  
   it 'has meat' do
-    burger = Burger.new('Beef', 'Cheddar')
     expect(burger.meat).to eq('Beef')
   end
 
   it 'has cheese' do
-    burger = Burger.new('Beef', 'Cheddar')
     expect(burger.cheese).to eq('Cheddar')
   end
 end
+
+# RSpec.describe Burger do
+#   it 'has meat' do
+#     burger = Burger.new('Beef', 'Cheddar')
+#     expect(burger.meat).to eq('Beef')
+#   end
+
+#   it 'has cheese' do
+#     burger = Burger.new('Beef', 'Cheddar')
+#     expect(burger.cheese).to eq('Cheddar')
+#   end
+# end
 
 # RSpec.describe 'Burger' do
 #   it 'has a type' do
