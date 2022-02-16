ae73c660eb09869a68857c67375cc28f4f196cac
JenJen <jennifer7793@gmail.com>
2022-02-16 11:04:57 +0800

    before and after(:context) works at very beginning and final

--- spec/bahooks_spec.rb
+++ spec/bahooks_spec.rb
@@ -1,19 +1,41 @@
 RSpec.describe "before and after hooks" do
   before(:example) do
+    p "before example"
+  end
+
+  before(:context) do
+    p "before context"
   end
 
   after(:example) do
-    p @array
-    p @array.clear
+    p "after example"
+  end
+
+  after(:context) do
+    p "after context"
   end
 
   it "test for before and after" do
-    @array = []
-    p "Inner #{@array}"
-    @array << '1'
     expect(1 + 1).to eq(2)
   end
 end
+
+# RSpec.describe "before and after hooks" do
+#   before(:example) do
+#   end
+
+#   after(:example) do
+#     p @array
+#     p @array.clear
+#   end
+
+#   it "test for before and after" do
+#     @array = []
+#     p "Inner #{@array}"
+#     @array << '1'
+#     expect(1 + 1).to eq(2)
+#   end
+# end
 
 # RSpec.describe "before and after hooks" do
 #   before(:example) do
