d06ef3463d1625748488a0ac76169ffc9deb97d8
JenJen <jennifer7793@gmail.com>
2022-02-16 10:36:29 +0800

    change before hook to let

--- spec/burger_spec.rb
+++ spec/burger_spec.rb
@@ -1,16 +1,31 @@
 RSpec.describe Burger do
-  before do 
-    @burger = Burger.new('Beef', 'Cheddar')
-  end
+  let(:burger) { Burger.new('Pork', 'Cheddar') }
   
   it 'has meat' do
-    expect(burger.meat).to eq('Beef')
+    comparison = "Beef"
+    expect(burger.meat).to eq(comparison), "I want #{comparison}, not #{burger.meat}"
   end
 
   it 'has cheese' do
     expect(burger.cheese).to eq('Cheddar')
+    burger.cheese = 'Ricotta'
+    expect(burger.cheese).to eq('Ricotta')
   end
 end
+
+# RSpec.describe Burger do
+#   before do 
+#     @burger = Burger.new('Beef', 'Cheddar')
+#   end
+  
+#   it 'has meat' do
+#     expect(burger.meat).to eq('Beef')
+#   end
+
+#   it 'has cheese' do
+#     expect(burger.cheese).to eq('Cheddar')
+#   end
+# end
 
 # RSpec.describe Burger do
 #   it 'has meat' do
