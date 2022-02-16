9857bab6c0ba9e1d365bfec873cedf9abef5b9a0
JenJen <jennifer7793@gmail.com>
2022-02-16 10:59:05 +0800

    use after hooks to clear records

--- spec/bahooks_spec.rb
+++ spec/bahooks_spec.rb
@@ -1,14 +1,31 @@
 RSpec.describe "before and after hooks" do
   before(:example) do
-    p "before hooks"
   end
 
   after(:example) do
-    p "after hooks"
+    p @array
+    p @array.clear
   end
 
   it "test for before and after" do
-    p "inner example"
+    @array = []
+    p "Inner #{@array}"
+    @array << '1'
     expect(1 + 1).to eq(2)
   end
 end
+
+# RSpec.describe "before and after hooks" do
+#   before(:example) do
+#     p "before hooks"
+#   end
+
+#   after(:example) do
+#     p "after hooks"
+#   end
+
+#   it "test for before and after" do
+#     p "inner example"
+#     expect(1 + 1).to eq(2)
+#   end
+# end
