1399ba9ff69a87206a485e4609173c0317c25e76
JenJen <jennifer7793@gmail.com>
2022-02-17 10:33:16 +0800

    original subject

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -1,7 +1,17 @@
-RSpec.describe Hash do
+RSpec.describe Hash do 
+  subject do 
+    {a: 3, b: 4}
+  end
+
   it 'should be empty' do
-    p subject
-    p subject.class
-    expect(subject.length).to eq(0)
+    expect(subject.length).to eq(2)
   end
 end
+
+# RSpec.describe Hash do
+#   it 'should be empty' do
+#     p subject
+#     p subject.class
+#     expect(subject.length).to eq(0)
+#   end
+# end
